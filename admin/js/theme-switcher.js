b320914ed504f69f637dfa96d51b8f1ef013baee
Joppe-VH <joppe.vanhoof@gmail.com>
2024-12-28 03:21:03 +0100

    add admin header

---
+++ admin/js/theme-switcher.js
@@ -0,0 +1,82 @@
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2024 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
+
+(() => {
+  "use strict";
+
+  const getStoredTheme = () => localStorage.getItem("theme");
+  const setStoredTheme = (theme) => localStorage.setItem("theme", theme);
+
+  const getPreferredTheme = () => {
+    const storedTheme = getStoredTheme();
+    if (storedTheme) {
+      return storedTheme;
+    }
+
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  };
+
+  const setTheme = (theme) => {
+    if (theme === "auto") {
+      document.documentElement.setAttribute(
+        "data-bs-theme",
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "dark"
+          : "light"
+      );
+    } else {
+      document.documentElement.setAttribute("data-bs-theme", theme);
+    }
+  };
+
+  setTheme(getPreferredTheme());
+
+  const showActiveTheme = (theme) => {
+    const themeSwitcher = document.querySelector(".theme-switcher");
+
+    if (!themeSwitcher) {
+      return;
+    }
+
+    switch (theme) {
+      case "dark":
+        themeSwitcher.classList.add("dark");
+        themeSwitcher.classList.remove("light");
+        break;
+      case "light":
+        themeSwitcher.classList.add("light");
+        themeSwitcher.classList.remove("dark");
+        break;
+      default:
+        themeSwitcher.classList.remove("dark");
+        themeSwitcher.classList.remove("light");
+    }
+  };
+
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", () => {
+      const storedTheme = getStoredTheme();
+      if (storedTheme !== "light" && storedTheme !== "dark") {
+        setTheme(getPreferredTheme());
+      }
+    });
+
+  window.addEventListener("DOMContentLoaded", () => {
+    showActiveTheme(getPreferredTheme());
+
+    document.querySelectorAll(".theme-switcher button").forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const theme = btn.getAttribute("data-ts-theme");
+        setStoredTheme(theme);
+        setTheme(theme);
+        showActiveTheme(theme);
+      });
+    });
+  });
+})();
---
+++ admin/partials/theme-switcher.inc.php
@@ -0,0 +1,12 @@
+<script src="/admin/js/theme-switcher.js" defer type="module"></script>
+<div class="theme-switcher">
+    <button data-ts-theme="dark">
+        <i class="fa-solid fa-moon"></i>
+    </button>
+    <button data-ts-theme="auto">
+        <i class="fa-solid fa-circle-half-stroke"></i>
+    </button>
+    <button data-ts-theme="light">
+        <i class="fa-solid fa-sun"></i>
+    </button>
+</div>
