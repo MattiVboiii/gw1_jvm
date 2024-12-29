8ceb0a035ba72a8f23f9400aa7f21592612db19e
Mattias Verlinden <mattiverlinden@hotmail.com>
2024-12-29 13:25:33 +0100

    added pagination, more styling etc

--- frontend/index.php
+++ frontend/index.php
@@ -28,19 +28,40 @@ include_once 'frontend/php_includes/func.inc.php';
             <img src="/frontend/images/sample.jpg" alt="">
             <ul>
                 <?php foreach (getClubs() as $club) : ?>
-                    <li><?= $club['name'] ?></li>
+                    <li><a href="#"><?= $club['name'] ?></a></li>
                 <?php endforeach; ?>
             </ul>
         </nav>
         <header>
             <p>BANNER - Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis harum quibusdam, accusantium magnam sapiente mollitia quae dicta, dolore consequuntur incidunt modi excepturi quod expedita saepe ipsa autem repellat suscipit nisi!</p>
         </header>
+        <?php
+        $sectionPerPage = 4;
+        $page = (int) ($_GET['page'] ?? 1);
+        $sections = getClubs();
+        $sectionsToShow = array_slice($sections, ($page - 1) * $sectionPerPage, $sectionPerPage);
+        ?>
         <div class="club-container">
-            <?php foreach (getClubs() as $club) : ?>
-                <section style="background: url('<?= $club['logo_url'] ?>') no-repeat center/contain; height: 250px;">
+            <?php foreach ($sectionsToShow as $club) : ?>
+                <section style="background-image: url('<?= $club['logo_url'] ?>');">
                     <h2><?= $club['name'] ?></h2>
                 </section>
             <?php endforeach; ?>
+        </div>
+        <div class="pagination">
+            <?php if ($page > 1) : ?>
+                <a href="?page=<?= $page - 1 ?>" class="prev">&lt;</a>
+            <?php else : ?>
+                <a class="prev" href="#" aria-disabled="true">&lt;</a>
+            <?php endif; ?>
+            <?php for ($i = 1; $i <= ceil(count($sections) / $sectionPerPage); $i++) : ?>
+                <a href="?page=<?= $i ?>" class="page-<?= $i ?> <?= $i == $page ? 'active' : '' ?>"><?= $i ?></a>
+            <?php endfor; ?>
+            <?php if ($page < ceil(count($sections) / $sectionPerPage)) : ?>
+                <a href="?page=<?= $page + 1 ?>" class="next">&gt;</a>
+            <?php else : ?>
+                <a class="next" href="#" aria-disabled="true">&gt;</a>
+            <?php endif; ?>
         </div>
         <footer>
             <p>FOOTER - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti voluptate, nulla repellat cupiditate molestias voluptas tenetur, vitae minima maxime delectus dolore. Excepturi voluptas maxime quam nesciunt sequi, dolore illo. Quam!</p>
