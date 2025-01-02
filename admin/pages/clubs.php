a84abee8c65f3c592ed0d0c99573f178f028d3dc
Joppe-VH <joppe.vanhoof@gmail.com>
2025-01-02 21:18:30 +0100

    extract common head in its own file

--- admin/pages/clubs.php
+++ admin/pages/clubs.php
@@ -9,21 +9,8 @@ $clubs = getClubs();
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require "admin/partials/head.inc.php" ?>
     <title>Admin - Clubs</title>
-    <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-        crossorigin="anonymous">
-    <script
-        defer
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous">
-    </script>
-    <link rel="stylesheet" href="/admin/css/style.css">
     <link rel="stylesheet" href="/admin/css/clubs.css">
     <script type="module" src="/admin/js/club-modals.js" defer></script>
 </head>
