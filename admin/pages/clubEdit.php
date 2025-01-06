5f9b87b3890fb772d7998c657e87090160b1d139
Joppe-VH <joppe.vanhoof@gmail.com>
2025-01-06 14:27:53 +0100

    update title admin pages

--- admin/pages/clubEdit.php
+++ admin/pages/clubEdit.php
@@ -268,7 +268,7 @@ $makeGetValidationClass = function ($isSubmitted) use ($errors) {
 
 <head>
     <?php require 'admin/partials/head.inc.php' ?>
-    <title>Admin - Clubs</title>
+    <title>Admin - Club:<?= $club['name'] ?></title>
     <link rel="stylesheet" href="/admin/css/clubForm.css">
     <script defer type="module" src="/admin/js/clubForm.js"></script>
     <script defer type="module" src="/admin/js/clubEdit-modal.js"></script>
