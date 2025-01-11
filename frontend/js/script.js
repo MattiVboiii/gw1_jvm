d63fd7e2cbb4c5acbd2973a7bcb70c5e906c23aa
Joppe-VH <joppe.vanhoof@gmail.com>
2025-01-12 00:27:41 +0100

    fix feature readmore

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,28 +1,19 @@
-console.log("javascript works on frontend.......");
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const readMoreBody = document.querySelector(".readmore-body");
-  const firstPart = readMoreBody.querySelector(".first");
   const secondPart = readMoreBody.querySelector(".second");
   const checkbox = document.querySelector("#readmore");
   const label = document.querySelector('label[for="readmore"]');
-
   const transition =
     window.getComputedStyle(readMoreBody).transitionDuration.split("s")[0] *
     1000;
   const temp = readMoreBody.querySelector(".temp");
 
-  // console.log("collapsed height", collapsedHeight);
-  console.log("readMoreBody.scrollH", readMoreBody.scrollHeight);
-  console.log("maxHeight", readMoreBody.style.maxHeight);
-  console.log(
-    "transition",
-    window.getComputedStyle(readMoreBody).transitionDuration.split("s")[0] *
-      1000
-  );
   let collapsedHeight;
-  checkbox.addEventListener("change", () => {
-    // console.log("collapsed height", collapsedHeight);
+  checkbox.addEventListener("change", async () => {
     if (checkbox.checked) {
       collapsedHeight = parseFloat(
         window.getComputedStyle(readMoreBody).height
@@ -32,14 +23,21 @@ document.addEventListener("DOMContentLoaded", () => {
       readMoreBody.style.maxHeight = `${readMoreBody.scrollHeight}px`; // Expand
       label.textContent = "Collapse";
       temp.style.display = "none";
+      await sleep(transition);
+      readMoreBody.style.maxHeight = "unset";
     } else {
+      const expandedHeight = parseFloat(
+        window.getComputedStyle(readMoreBody).height
+      );
+      readMoreBody.style.maxHeight = expandedHeight + "px";
+      console.log("expandedHeight", expandedHeight + "px");
+      await sleep(1);
       readMoreBody.style.maxHeight = `${collapsedHeight}px`; // Collapse
       label.textContent = "Read more";
-      setTimeout(() => {
-        secondPart.style.display = "none";
-        temp.style.display = "inline";
-        readMoreBody.style.maxHeight = "unset";
-      }, transition);
+      await sleep(transition);
+      secondPart.style.display = "none";
+      temp.style.display = "inline";
+      readMoreBody.style.maxHeight = "unset";
     }
   });
 });
