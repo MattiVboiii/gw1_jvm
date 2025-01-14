b0c8d025c17420f3f53207656b1b5606203e44ee
Joppe-VH <joppe.vanhoof@gmail.com>
2025-01-14 06:57:56 +0100

    add vite url rewriting

--- vite.config-with-php.js
+++ vite.config-with-php.js
@@ -20,10 +20,33 @@ export default {
           return;
         }
 
-        // requestUrl.search = '_request_=' + requestUrl.pathname;
-        // requestUrl.pathname = 'index.php';
-
-        return requestUrl;
+        switch (true) {
+          case "" == requestUrl.pathname:
+            return "/frontend/index.php";
+          case requestUrl.pathname.startsWith("/club/"):
+            requestUrl.search = `?id=${requestUrl.pathname.slice(6)}`;
+            requestUrl.pathname = "/frontend/pages/detail.php";
+            return requestUrl;
+          case "/admin" == requestUrl.pathname:
+            requestUrl.pathname = "/backend/pages/home.php";
+            return requestUrl;
+          case "/admin/login" == requestUrl.pathname:
+            requestUrl.pathname = "/backend/pages/login.php";
+            return requestUrl;
+          case "/admin/clubs" == requestUrl.pathname:
+            requestUrl.pathname = "/backend/pages/clubs.php";
+            return requestUrl;
+          case requestUrl.pathname.startsWith("/admin/clubs/edit/"):
+            requestUrl.search = `?id=${requestUrl.pathname.slice(18)}`;
+            requestUrl.pathname = "/backend/pages/clubEdit.php";
+            return requestUrl;
+          case "/admin/clubs/create" == requestUrl.pathname:
+            requestUrl.pathname = "/backend/pages/clubCreate.php";
+            return requestUrl;
+          case "/404" == requestUrl.pathname:
+            requestUrl.pathname = "/frontend/pages/404.php";
+            return requestUrl;
+        }
       },
     }),
     viteStaticCopy({
