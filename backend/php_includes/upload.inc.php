e8a62e713e7504f7ac864b0d72cf02930afa425a
Joppe-VH <joppe.vanhoof@gmail.com>
2025-01-14 04:35:29 +0100

    fix type in include

--- backend/php_includes/upload.inc.php
+++ backend/php_includes/upload.inc.php
@@ -3,4 +3,4 @@
 require_once 'backend/php_includes/Site/Admin/FileSizeUnits.php';
 require_once 'backend/php_includes/Site/Admin/MimeType.php';
 require_once 'backend/php_includes/Site/Admin/UploadError.php';
-require_once 'backend/php_includes/Site/Admin/upload.php';
+require_once 'backend/php_includes/Site/Admin/Upload.php';
